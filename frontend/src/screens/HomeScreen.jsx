8cc965978fba6d253ac8e0686b39df3ab68c06a3
Copheles <xixxykitkit@gmail.com>
2024-02-22 14:10:26 +0630

    Added Product API join with frontend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,32 @@
-import { Col, Row } from "react-bootstrap"
-import products from "../products"
-import Product from "../components/Product"
+import { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts()
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        { products.map((product) => (
+        {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} key={product._id}/>
+            <Product product={product} key={product._id} />
           </Col>
         ))}
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
