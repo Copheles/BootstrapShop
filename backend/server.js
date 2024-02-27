daf2f24e1ad569e81a66473635d816b3e3eff6a3
Copheles <xixxykitkit@gmail.com>
2024-02-27 10:20:36 +0630

    Change fetching logic with redux toolkit query

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
+// Routes
 import productRoutes from './routes/productRoutes.js';
+
+// Error Handler
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
-connectDB() // Connect tp MongoDb
+connectDB() // Connect to MongoDb
 
 
 const port = process.env.PORT || 5000;
@@ -19,6 +23,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use(notFound);
+app.use(errorHandler)
 
 
 app.listen(port, () => console.log(`Server running on port:${port}`))
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,92 +2,78 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useGetProductQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const [product, setProduct] = useState();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-
-    fetchProduct();
-  }, [productId]);
-
-  console.log(product)
+  const { data: product, error, isLoading } = useGetProductQuery(productId);
 
   return (
     <>
-      {product && (
-        <>
-          <Link className="btn btn-light my-3" to="/">
-            Go Back
-          </Link>
-
-          <Row>
-            <Col md={5}>
-              <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={4}>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {
+        isLoading ? (<div>Loading....</div>) : error ? (<div>{error.data?.message || error.error}</div>): (<Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} ${
+                    product.numReviews > 1 ? "reviews" : "review"
+                  }`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Description: </strong> {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <h3>{product.name}</h3>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} ${
-                      product.numReviews > 1 ? "reviews" : "review"
-                    }`}
-                  />
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <strong>Description: </strong> {product.description}
+                  <Button
+                    className="btn-dark"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
                 </ListGroup.Item>
               </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${product.price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        <strong>
-                          {product.countInStock > 0
-                            ? "In Stock"
-                            : "Out Of Stock"}
-                        </strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Button
-                      className="btn-dark"
-                      type="button"
-                      disabled={product.countInStock === 0}
-                    >
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          </Row>
-        </>
-      )}
+            </Card>
+          </Col>
+        </Row>)
+      }
+
+      
     </>
   );
 };
